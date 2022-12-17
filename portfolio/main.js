e54e0fa8261eac268b691f685e3a2282ae7bb923
himanshu1856 <hs287190@gmail.com>
2022-12-17 18:08:51 +0530

    Reverse text added

--- portfolio/main.js
+++ portfolio/main.js
@@ -4,15 +4,27 @@ const textArray = ["Developer"]
 const text = textArray[0]
 let content = ""
 let index = 0
-function handleContent(c,text){
+
+function handleDelete(c,text){
+    if(index == -1){
+        return
+    }
+    index--;
+    c = c.substring(0,index -1)
+    dynamicspan.textContent = c
+    setTimeout(handleDelete,500,c,text)
+}
+
+function handlePrint(c,text){
     if(index == text.length){
+        handleDelete(c,text)
         return
     }
     c += text[index]
     dynamicspan.textContent = c
     index++
-    setTimeout(handleContent,500,c,text)
+    setTimeout(handlePrint,500,c,text)
 }
 
-handleContent(content,text)
+handlePrint(content,text)
 
