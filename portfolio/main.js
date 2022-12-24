4de76b10fce3a59a40aa1439440eb7917eacb5c0
himanshu1856 <hs287190@gmail.com>
2022-12-24 12:33:09 +0530

    navbar implemented

--- portfolio/main.js
+++ portfolio/main.js
@@ -1,44 +1,48 @@
-function handleDelete(){
-    if(index == -1){   
-        index = 0
-        arrayIdx++;
-        iterateOverArray()
-        console.log("Inside if")
-        return
+document.addEventListener('DOMContentLoaded', () => {
+
+    function handleDelete() {
+        if (index == -1) {
+            index = 0
+            arrayIdx++;
+            iterateOverArray()
+            console.log("Inside if")
+            return
+        }
+        index--;
+        content = content.substring(0, index - 1)
+        dynamicspan.textContent = content
+        setTimeout(handleDelete, 200)
     }
-    index--;
-    content = content.substring(0,index -1)
-    dynamicspan.textContent = content
-    setTimeout(handleDelete,200)
-}
 
-function handlePrint(text){
-    if(index == text.length){
-        handleDelete(text)
-        return
+    function handlePrint(text) {
+        if (index == text.length) {
+            handleDelete(text)
+            return
+        }
+        content += text[index]
+        dynamicspan.textContent = content
+        index++
+        setTimeout(handlePrint, 200, text)
     }
-    content += text[index]
-    dynamicspan.textContent = content
-    index++
-    setTimeout(handlePrint,200,text)
-}
 
-function iterateOverArray(){
-    if(arrayIdx == phrases.length){
-        arrayIdx = 0
+    function iterateOverArray() {
+        if (arrayIdx == phrases.length) {
+            arrayIdx = 0
+        }
+        let text = phrases[arrayIdx]
+        handlePrint(text)
     }
-    let text = phrases[arrayIdx]
-    handlePrint(text)
-}
 
-const dynamicspan = document.querySelector("#dynamic-text")
+    const dynamicspan = document.querySelector("#dynamic-text")
 
-const phrases = ["I am a Web Developer...","I am pursuing B.Tech..."]
-let content = ""
-let index = 0
-let arrayIdx = 0
+    const phrases = ["I am a Web Developer...", "I am pursuing B.Tech..."]
+    let content = ""
+    let index = 0
+    let arrayIdx = 0
 
-iterateOverArray()
+    iterateOverArray()
+
+})
 
 
 
